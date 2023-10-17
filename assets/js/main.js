a5c852e94969249233466c0271df550ad9ac4de0
mmozos <martinmozos@gmail.com>
2023-10-17 18:13:49 +0200

    Upgrade dependencies, simplify code about colors and add animated social media dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,14 +59,14 @@ window.addEventListener('scroll', scrollHeader);
     centeredSlides: true,
     slidesPerView: 'auto',
     loop: 'true',
-    spaceBetween: 16,
+    spaceBetween: 16
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
-    const scrollY = window.pageYOffset;
+    const scrollY = window.scrollY;
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
@@ -107,3 +107,91 @@ sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
 sr.reveal(`.category__data, .trick__content, .footer__content`, {interval: 100});
 sr.reveal(`.about__data, .discount__img`, {origin: 'left'});
 sr.reveal(`.about__img, .discount__data`, {origin: 'right'});
+
+/*=============== SOCIAL FOOTER ===============*/ // @see https://codepen.io/zeeshanmukhtar1/pen/ExGGMgw
+const root = document.documentElement;
+const dropdownTitleIcon = document.querySelector(".dropdown-title-icon");
+const dropdownTitle = document.querySelector(".dropdown-title");
+const dropdownList = document.querySelector(".dropdown-list");
+const mainButton = document.querySelector(".main-button");
+const floatingIcon = document.querySelector(".floating-icon");
+
+const icons = {
+  facebook: 'bx bxl-facebook',
+  instagram: 'bx bxl-instagram',
+  linkedin: 'bx bxl-linkedin',
+  reddit: 'bx bxl-reddit',
+  tiktok: 'bx bxl-tiktok',
+  twitter: 'bx bxl-twitter',
+  youtube: 'bx bxl-youtube'
+};
+
+const listItems = ["Facebook", "Instagram", "Linkedin", "Reddit", "TikTok", "Twitter", "Youtube"];
+
+const iconTemplate = (icon) => {
+  return `<i class="${icon}"></i>`;
+};
+
+const listItemTemplate = (text, translateValue) => {
+  return `
+    <li class="dropdown-list-item">
+      <button class="dropdown-button list-button" data-translate-value="${translateValue}%">
+        <span class="text-truncate">${text}</span>
+      </button>
+    </li>
+  `;
+};
+
+const renderListItems = () => {
+  dropdownList.innerHTML += listItems
+    .map((item, index) => {
+      return listItemTemplate(item, 100 * index);
+    })
+    .join("");
+};
+
+window.addEventListener("load", () => {
+  renderListItems();
+});
+
+const setDropdownProps = (deg, ht, opacity) => {
+  root.style.setProperty("--rotate-arrow", deg !== 0 ? deg + "deg" : 0);
+  root.style.setProperty("--dropdown-height", ht !== 0 ? ht + "rem" : 0);
+  root.style.setProperty("--list-opacity", opacity);
+};
+
+mainButton.addEventListener("click", () => {
+  const listWrapperSizes = 3.5; // margins, paddings & borders
+  const dropdownOpenHeight = 4.6 * listItems.length + listWrapperSizes;
+  const currDropdownHeight =
+    root.style.getPropertyValue("--dropdown-height") || "0";
+  currDropdownHeight === "0"
+    ? setDropdownProps(180, dropdownOpenHeight, 1)
+    : setDropdownProps(0, 0, 0);
+});
+
+dropdownList.addEventListener("mouseover", (e) => {
+  const translateValue = e.target.dataset.translateValue;
+  root.style.setProperty("--translate-value", translateValue);
+});
+
+dropdownList.addEventListener("click", (e) => {
+  const clickedItemText = e.target.innerText.toLowerCase().trim();
+  const clickedItemIcon = icons[clickedItemText];
+
+  dropdownTitleIcon.innerHTML = iconTemplate(clickedItemIcon);
+  dropdownTitle.innerHTML = clickedItemText;
+  setDropdownProps(0, 0, 0);
+});
+
+dropdownList.addEventListener("mousemove", (e) => {
+  const iconSize = root.style.getPropertyValue("--floating-icon-size") || 0;
+  const x = e.clientX - dropdownList.getBoundingClientRect().x;
+  const y = e.clientY - dropdownList.getBoundingClientRect().y;
+  const targetText = e.target.innerText.toLowerCase().trim();
+  const hoverItemText = icons[targetText];
+
+  floatingIcon.innerHTML = iconTemplate(hoverItemText);
+  root.style.setProperty("--floating-icon-left", x - iconSize / 2 + "px");
+  root.style.setProperty("--floating-icon-top", y - iconSize / 2 + "px");
+});
