382d5eb3b5c127b8710601b5442bf5963d3f6ac9
mmozos <martinmozos@gmail.com>
2023-10-28 17:04:56 +0200

    TODO: Translations

--- assets/js/responsive-halloween-website.js
+++ assets/js/responsive-halloween-website.js
@@ -10,97 +10,97 @@ const navClose = document.getElementById('nav-close');
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-    navToggle.addEventListener('click', () => navMenu.classList.add('show-menu'));
+  navToggle.addEventListener('click', () => navMenu.classList.add('show-menu'));
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-    navClose.addEventListener('click', () => navMenu.classList.remove('show-menu'));
+  navClose.addEventListener('click', () => navMenu.classList.remove('show-menu'));
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu');
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== HOME SWIPER ===============*/
 /*let homeSwiper = */new Swiper('.home-swiper', {
-    autoplay: {
-        delay: 4000
-    },
-    loop: 'true',
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    spaceBetween: 30
+  autoplay: {
+    delay: 4000
+  },
+  loop: 'true',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  spaceBetween: 30
 })
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
-    const header = document.getElementById('header');
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 50) {
-        header.classList.add('scroll-header');
-    } else {
-        header.classList.remove('scroll-header');
-    }
+  const header = document.getElementById('header');
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
 }
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== NEW SWIPER ===============*/
 /*let newSwiper = */new Swiper('.new-swiper', {
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    loop: 'true',
-    spaceBetween: 16
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: 'true',
+  spaceBetween: 16
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
-    const scrollY = window.scrollY;
+  const scrollY = window.scrollY;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 58;
-        const sectionId = current.getAttribute('id');
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  })
 }
 window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 460 viewport height, add the show-scroll class to the anchor tag with the scroll-top class
-    if (this.scrollY >= 460) {
-        scrollUp.classList.add('show-scroll');
-    } else {
-        scrollUp.classList.remove('show-scroll');
-    }
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the anchor tag with the scroll-top class
+  if (this.scrollY >= 460) {
+    scrollUp.classList.add('show-scroll');
+  } else {
+    scrollUp.classList.remove('show-scroll');
+  }
 }
 window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400/*,
-    reset: true*/
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400/*,
+  reset: true*/
 });
 
 sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
@@ -159,13 +159,11 @@ const iconTemplate = (icon) => {
 };
 
 const listItemTemplate = (text, translateValue) => {
-  return `
-    <li class="dropdown-list-item">
-      <button class="dropdown-button list-button" data-translate-value="${translateValue}%">
-        <span class="text-truncate">${text}&nbsp;${iconTemplate(icons[text.toLowerCase()])}</span>
-      </button>
-    </li>
-  `;
+  return `<li class="dropdown-list-item">
+    <button class="dropdown-button list-button" data-translate-value="${translateValue}%">
+    <span class="text-truncate">${text}&nbsp;${iconTemplate(icons[text.toLowerCase()])}</span>
+    </button>
+  </li>`;
 };
 
 const renderListItems = () => {
@@ -189,11 +187,10 @@ const setDropdownProps = (deg, ht, opacity) => {
 mainButton.addEventListener('click', () => {
   const listWrapperSizes = 3.5; // margins, paddings & borders
   const dropdownOpenHeight = 4.6 * listItems.length + listWrapperSizes;
-  const currDropdownHeight =
-    root.style.getPropertyValue('--dropdown-height') || '0';
-  currDropdownHeight === '0'
-    ? setDropdownProps(180, dropdownOpenHeight, 1)
-    : setDropdownProps(0, 0, 0);
+  const currDropdownHeight = root.style.getPropertyValue('--dropdown-height') || '0';
+  currDropdownHeight === '0' ?
+    setDropdownProps(180, dropdownOpenHeight, 1) :
+    setDropdownProps(0, 0, 0);
 });
 
 dropdownList.addEventListener('mouseover', (e) => {
@@ -226,4 +223,3 @@ dropdownList.addEventListener('mousemove', (e) => {
   root.style.setProperty('--floating-icon-left', x - iconSize / 2 + 'px');
   root.style.setProperty('--floating-icon-top', y - iconSize / 2 + 'px');
 });
-
